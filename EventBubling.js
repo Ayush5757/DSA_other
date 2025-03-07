a621e24e56cb226b7d4ff6a0ac2ff62561f00991
Aayush Mishra <50010917+Ayush5757@users.noreply.github.com>
2025-03-07 12:58:51 +0530

    some changes added

--- EventBubling.js
+++ EventBubling.js
@@ -1,10 +1,10 @@
-console.log('Event Bubling');
+console.log('Event Bubling started');
 
 var div = document.querySelector('div')
 var btn = document.querySelector('button')
 
 div.addEventListener('click',(event)=>{    
-    console.dir(event.target.innerHTML);
+    console.dir(event.target.innerHTML)
 })
 
 
